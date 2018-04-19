cf5a40f1021a6d4914a4bd0dec0c26986fa156bb
unknown <reyes_palencia_romero@hotmail.com>
2018-04-19 16:38:29 +0200

    first commit

---
+++ src/main/java/es/bueno/rp/es/bueno/rp/App.java
@@ -0,0 +1,13 @@
+package es.bueno.rp.es.bueno.rp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
